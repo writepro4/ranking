a035a5bed724eb3e4b61b7bd82e9a766a3204cf4
writepro4 <writepro4@naver.com>
2020-06-28 20:56:00 +0900

    Updates

---
+++ precache-manifest.9c393507a6e0c8be463e9e10b3fbbaf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ea83ced7aded5469ae85b5090e5a99",
+    "url": "/ranking/index.html"
+  },
+  {
+    "revision": "d219d26bef30dd2b9e98",
+    "url": "/ranking/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "825af1bb8907a10f974e",
+    "url": "/ranking/static/js/2.9e11718d.chunk.js"
+  },
+  {
+    "revision": "d219d26bef30dd2b9e98",
+    "url": "/ranking/static/js/main.59858c42.chunk.js"
+  },
+  {
+    "revision": "52329a681bf1cbdebeeb",
+    "url": "/ranking/static/js/runtime~main.eedecc46.js"
+  }
+]);
